c543526b80258ce49422c4b3ddb0e347e91c2dbb
PhilipVigus <philipvigus@yahoo.co.uk>
2020-10-01 14:08:24 +0100

    Add test for navigating to second component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter as Router } from "react-router-dom";
 import App from "./App";
 
 describe("App", () => {
@@ -28,6 +28,21 @@ describe("App", () => {
 
     expect(
       screen.getByRole("heading", { name: "First component" })
+    ).toBeInTheDocument();
+  });
+
+  it("navigates to the first component", () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    const link = screen.getByRole("link", { name: "Go to second component" });
+    fireEvent.click(link);
+
+    expect(
+      screen.getByRole("heading", { name: "Second component" })
     ).toBeInTheDocument();
   });
 });
